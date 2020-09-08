b7bc0e248a1fc70b88b47061040de3902d88934a
Ortiz David <49447702+soporte00@users.noreply.github.com>
2020-09-08 01:19:31 -0300

    improving js

--- alert.js
+++ alert.js
@@ -1,19 +1,30 @@
 const alertBox = document.getElementById('alertBox');
 
 function mssg(mssg,type=null){
-	const createMssg = document.createElement("div");
+	const createMssg = document.createElement("DIV");
 	
-	if(type == 'e' || type == 'error'){	createMssg.setAttribute("class", "alertWrong"); }else{ createMssg.setAttribute("class", "alertFine"); }
+	if(type === 'e' || type === 'error'){	
+		createMssg.setAttribute("class", "alertWrong"); 
+	}else{ 
+		createMssg.setAttribute("class", "alertFine"); 
+	}
 
-		createMssg.innerHTML =  mssg +"<button class='alertBox_btn' onclick=\"mssgRemove(this.parentNode)\">x</button>";	
+	createMssg.innerHTML = `<button class='alertBox_btn'>x</button> ${mssg}`;	
 
 	alertBox.appendChild(createMssg);
 
+
+	setTimeout(function(){ 	
+		if(alertBox.contains(createMssg)){ createMssg.classList.add('alertHidden') }
+	}, 5300);
+
+
 	setTimeout(function(){ 	
 		if(alertBox.contains(createMssg)){ alertBox.removeChild(createMssg); }
-	}, 5000);
-}	
+	}, 6000);
 
-function mssgRemove(e){
-	alertBox.removeChild(e);
+
+	createMssg.firstChild.addEventListener('click',(e)=>{
+		alertBox.removeChild(e.target.parentNode)
+	})
 }
